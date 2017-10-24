df8e0323976fc0690e7d09d09fd0278c0b023a1e
Christine Urban <christineurbansf@gmail.com>
2017-10-24 22:40:58 +0000

    added solutions

---
+++ javascript/beginner-series-number-1-school-paperwork.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/beginner-series-number-1-school-paperwork
+
+function paperwork(n, m) {
+  if(n<0 || m<0) return 0;
+  return n*m;
+}
---
+++ javascript/dollars-and-cents.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/dollars-and-cents
+
+function formatMoney(amount){
+  return "$" + String(amount.toFixed(2));
+}
